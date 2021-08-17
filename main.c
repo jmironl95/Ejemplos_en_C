6a0ee722a9e9c307a8f08bbe3d2442b2105e3f1b
jmironl95 <jmiron95@gmail.com>
2021-08-17 18:05:07 -0500

    Código Hola Mundo

---
+++ main.c
@@ -0,0 +1,17 @@
+/*comentarios son para nosotros
+nombre del programa
+Autor: Jorge Mirón 
+Modificado por:
+*/
+
+/*BIBLIOTECAS*/
+#include <stdio.h>
+
+/*DECLARACIÓN DE VARIABLES CON EL TIPO CORRESPONDIENTE*/
+
+/*ESTRCUTURA MAIN*/
+
+void main(){
+    printf("Hola UAM");
+    return 0;
+}
